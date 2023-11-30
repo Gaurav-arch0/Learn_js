7ed923138aeca64f3a847644681038815bc6f36c
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2023-11-30 11:27:25 +0530

    8_Nums_Maths.js

--- 8_Nums_Maths.js
+++ 8_Nums_Maths.js
@@ -1,2 +1,6 @@
 const score = 400;
-console.log(score);
+console.log(score);//400
+
+const balance = new Number(100);
+console.log(balance);//[Number:100]
+console.log(balance.toString());//100 ~ String
