18f4e1943e8a1f02bccf10e3239a774fd9b049c0
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-01-31 11:22:54 +0530

    10_Arrays.js

---
+++ 10_Arrays.js
@@ -0,0 +1,8 @@
+// ARRAYS - is a way of storing multiple items under a single variable name and has members for performing common array operations.
+// Javascript arrays are resizeable.
+// When we do array copy operation , the new copy is a reference to the original data. This is known as Shallow Copy.
+// Shallow Copy -  copy of an object whose properties share the same reference point.
+// Deep Copy - copy of an object whose properties do not share the same reference point.
+
+const array1 = [1,2,3,4];
+console.log(array1[0]);
