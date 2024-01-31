514c45b35ee7946d16526e0d9dc3b82fc0730dbc
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-01-31 10:07:20 +0530

    9_Dates.js

--- 9_Dates.js
+++ 9_Dates.js
@@ -1,5 +1,8 @@
 // Javascript Dates represent a single moment in time in a platform independent format.
 // Javascript object encapsulate an integral number that represents millisecondssince the midnight at 1st January 1970 UTC .
+// Later on when we have Temporal as a  standard, the Date object will be replaced with it.      
+// The main difference between Date and Temporal is that Temporal has more features like TimeZone API which can make working with dates easier. 
+// Date is an object in javascript.
 
 let mydate = new Date();
 console.log(mydate.toString()); // thu nov 30 2023 16:18:11 gmt+0530 (Indian standard time)
@@ -15,7 +18,7 @@ console.log(mydate.toLocaleString());// 30/11/2023, 4:21:32 pm
 // These are some functions of js to get date and time in different formats in js .
 
 let myCreatedDate = new Date(2023,0,23);
-console.log(myCreatedDate.toDateString()); // Mon Jan 23 2023 ~ Here we understand that month is indexed with 0 .
+console.log(myCreatedDate.toDateString()); // Mon Jan 23 2023 ~ Here we understand that month is indexed with 0 only when writing in array  , If we want to print date structure as("YYYY-MM-DD"/"MM-DD-YYYY") month should be 01.
 
 
 let mynewDate = new Date(2023,0,23,5,3);// ~ (Year,Month,Date,Hours,Minutes)// 23/1/2023, 5:03:00 am ~ (Year,Month,Date,Hours,Minutes)
