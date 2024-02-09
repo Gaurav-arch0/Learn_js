e09751fb7f4f031b8c2e0ab28fa06cc68c43f04c
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-02-09 16:43:09 +0530

    12_Objects_Destructure.js

--- 12_Objects_De_structure.js
+++ 12_Objects_De_structure.js
@@ -15,7 +15,7 @@ console.log(instruct);
 // const navbar = ({company}) => {
 
 //     // }
-// navbar(company = "hitesh")// This is the same way of destructuring  assignment we did above,  But here instead of taking all properties of company object ,we are giving a default value for company which
+// navbar(company = "hitesh")    // This is the same way of destructuring  assignment we did above,  But here instead of taking all properties of company object ,we are giving a default value for company which
 //     return (
         
 //         <div className="navbar">
@@ -26,3 +26,24 @@ console.log(instruct);
        
 //     )
 // }
+
+// JavaScript 
+//API - Application Programming Interface, it's like the back end of an application. It allows different software applications to communicate with each other.Before we used to get API in two formats XML and JSON.
+
+//JSON FORMAT
+// {
+//     "name":"John Doe",
+//     "age":30,
+//     "city":"New York"
+// }
+// In JSON format the code is given in key value pair in which both key and values are inputted as string.
+//[
+//     {},
+//     {},
+// ] //Array Format
+// Fetch Method 
+// fetch("https://api.github.com/users/angela4w")
+// .then((response) => response.json())
+// .then((data) => console.log(data))
+
+// This is how we can get response for json api Request.
