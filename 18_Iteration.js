a5d830fb6786650ef698bec9ae1e75eed3aaf023
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-02-27 15:12:22 +0530

    18_Iteration.js

--- 18_Iteration.js
+++ 18_Iteration.js
@@ -1,2 +1,16 @@
 // FOR Loop - Repeat a block of code for as long as the condition is true.
 // The loop will execute at least once, even if the condition is false.
+
+for(let i = 0 ; i<= 10 ; i++)
+{
+    const element = i;
+    console.log(element);
+}
+for(let i = 0 ; i<= 10 ; i++)
+{
+    const element = i;
+    if(element == 5){
+        console.log("5 is the best no.");
+    }
+    console.log(element);
+}
