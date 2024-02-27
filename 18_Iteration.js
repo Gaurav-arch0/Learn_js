64e6e47b28415f7b68477b141dcc570ba0667713
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-02-27 16:08:33 +0530

    18_Iteration.js

--- 18_Iteration.js
+++ 18_Iteration.js
@@ -12,5 +12,20 @@ for(let i = 0 ; i<= 10 ; i++)
     if(element == 5){
         console.log("5 is the best no.");
     }
-    console.log(element);
+    // console.log(element);
+}
+
+for(let i = 0 ; i <= 10 ; i++)
+{
+    console.log(`Outer Loop : ${i}`);
+    for(let j=0 ; j <= 10 ; j++)
+    //console.log(`Inner Loop value ${j} in Inner Loop ${i}`);
+    console.log(`${i} *  ${j}  = ${i*j} `);
+}
+
+let myarr = ["flash", "batman" , "superman"];
+console.log(myarr.length);
+for(let k = 0 ; k < myarr.length ; k++){
+    const ele = myarr[k];
+    console.log(ele);
 }
