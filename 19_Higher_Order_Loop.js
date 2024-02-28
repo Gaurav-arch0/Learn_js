7d7caa934ef85adb8b7ba8f1a9888e9fbab4dbbd
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-02-28 18:14:52 +0530

    19_Higher_Order_Loop.js

--- 19_Higher_Order_Loop.js
+++ 19_Higher_Order_Loop.js
@@ -112,6 +112,10 @@ myCoding.forEach( (item) => {
 }) // This is how we loop through values inside an array containing object .
 
 
+/*********************************************************************/
+
+// Filter , Map , Reduce
+
 // Filter -  It will return new array with the elements that satisfy the condition provided in filter() method.Whereas, foreach doesn't returns values.
 
 const myNums = [1,2,3,4,5,6,7,8,9,10]
@@ -139,3 +143,26 @@ myNums2.forEach( (num) => {
     }
 })
 console.log(newNums2);
+
+// More Exercises with Filter
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+const userBooks = books.filter( (bk) => bk.genre === 'Non-Fiction')
+console.log(userBooks);
+const userBooks1 = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre ===  "History" 
+})
+console.log(userBooks1);
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+const newNumss = myNumbers.map( (num) => num + 10)
+console.log(newNumss);
