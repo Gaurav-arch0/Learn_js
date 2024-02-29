63eeb3b0e3a9090f09bbc5418781ebf4cc1718af
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-02-29 12:13:43 +0530

    19_Higher_Order_Loop.js

--- 19_Higher_Order_Loop.js
+++ 19_Higher_Order_Loop.js
@@ -164,5 +164,14 @@ const userBooks1 = books.filter( (bk) => {
 console.log(userBooks1);
 
 const myNumbers = [1,2,3,4,5,6,7,8,9,10]
-const newNumss = myNumbers.map( (num) => num + 10)
+// const newNumss = myNumbers.map( (num) => num + 10) 
+// [
+//   11, 12, 13, 14, 15,
+//   16, 17, 18, 19, 20
+// ]
+
+const newNumss = myNumbers.map((num) => num*10)
+                          .map( (num) => num + 1)
+                          .filter((num) => num >= 40)
+
 console.log(newNumss);
