c677471a203447d8c76f7b384aa3433d74a4b4ca
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-02-29 17:33:51 +0530

    DOM

---
+++ DOM/Dom.js
@@ -0,0 +1,19 @@
+document.getElementById('title'); // <h1 id="title" class="heading"> DOM Learning on Learn.js</h1>
+
+
+document.getElementById('title').id;  // "title"
+
+
+document.getElementById('title').class  // undefined
+
+
+document.getElementById('title').className // 'heading';
+
+
+document.getElementById('title').getAttribute('id') // 'title ';
+
+
+document.getElementById('title').getAttribute('class') // 'heading';
+
+
+
