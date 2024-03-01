612d62260d6af06cdc23ca98c64e0dbdce36e588
Gaurav Ahuja <gaurav07ahuja@gmail.com>
2024-03-01 15:37:14 +0530

    DOM

--- DOM/1_Dom.js
+++ DOM/1_Dom.js
@@ -1,19 +1,34 @@
-document.getElementById('title'); // <h1 id="title" class="heading"> DOM Learning on Learn.js</h1>
+// document.getElementById('title'); // <h1 id="title" class="heading"> DOM Learning on Learn.js</h1>
 
 
-document.getElementById('title').id;  // "title"
+// document.getElementById('title').id;  // "title"
 
 
-document.getElementById('title').class  // undefined
+// document.getElementById('title').class  // undefined
 
 
-document.getElementById('title').className // 'heading';
+// document.getElementById('title').className // 'heading';
 
 
-document.getElementById('title').getAttribute('id') // 'title ';
+// document.getElementById('title').getAttribute('id') // 'title ';
 
 
-document.getElementById('title').getAttribute('class') // 'heading';
+// document.getElementById('title').getAttribute('class') // 'heading';
 
 
+// document.getElementById('title').setAttribute('class','test')
 
+// title.style.backgroundColor = 'green'
+// 'green'
+// title.style.padding = '15px'
+// '15px'
+// title.style.borderRadius = '5px'
+// '5px'
+
+
+// title.textContent
+// ' DOM Learning on Learn.js test text'
+// title.innerText
+// 'DOM Learning on Learn.js'
+// title.innerHTML
+// ' DOM Learning on Learn.js <span style="display: none;">test text</span>'
